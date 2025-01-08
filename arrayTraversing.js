812a9b9b584a4c8f28c940a4047943e4c315a111
vatsal555 <prajapativatsal555@gmail.com>
2025-01-08 19:14:17 +0530

    array traversing

---
+++ arrayTraversing.js
@@ -0,0 +1,9 @@
+let arr = [9, 23, 45, 10, 27, 35, 67, 89, 99, 14, 69];
+
+// for (let i = 0; i < arr.length; i++) {
+//   document.write(`Array ${i} is ${arr[i]} <br>`);
+// }
+
+let x = 3;
+
+document.write(arr[3]);
